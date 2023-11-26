8bb67f4b2e23e519d255b81aeceada00f8ab0a49
ZiyouddinMominov <muminovz2009@gmail.com>
2023-11-26 15:51:48 +0500

    uyga vazifa muminov ziyovuddin

---
+++ js/app.js
@@ -0,0 +1,111 @@
+// 1 - TAYYOR
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi insonni yoshini kiriting");
+// if (a > b) {
+//   console.log(a ** 2);
+// } else {
+//   console.log(b ** 2);
+// }
+
+// 2 - TAYYOR
+// let a = +prompt("Birinchi sonni kiriting");
+// let b = +prompt("Ikkinchi sonni kiriting");
+// let c = +prompt("Uchinchi sonni kiriting");
+
+// if (a > b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+//   if (b > c) {
+//     console.log(b);
+//   } else {
+//     console.log(c);
+//   }
+// }
+
+// 3 - TAYYOR
+// let son = +prompt("Son kiriting");
+
+// if (son % 2 == 0) {
+//   console.log("Juft son");
+// } else {
+//   console.log("Toq son");
+// }
+
+// 4 - TAYYOR
+// let son = +prompt("Son kiriting men uni 5ga karraligini qaniqlab beraman");
+
+// if (son / 5 == 0) {
+//   console.log("5ga karrali");
+// } else {
+//   console.log("5ga karrali emas");
+// }
+
+// 5 - TAYYOR
+// let son = +prompt("son kiriting men uni 2 xonali ekanligini aniqlab beraman");
+// if (son > 9) {
+//   console.log(`Bu 2 xonali son: ${son}`);
+// } else {
+//   console.log(`Bu 2 xonali son emas: ${son}`);
+// }
+
+// 6 - TAYYOR
+// let son = +prompt("Son kiriting men uni 1 xonali ekanligini aniqlab beraman");
+
+// if (son > 11) {
+//   console.log(`Bu bir xonali son emas: ${son}`);
+// } else {
+//   console.log(`Bu bir xonali son: ${son}`);
+// }
+
+// 7 - TAYYOR
+// let son = +prompt(
+//   "Son kiriting men uni bir xonali sondan katta yoki kichikligini aniqlab beraman"
+// );
+// if (son > 9) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 8 - TAYYOR
+// let son = +prompt(
+//   "Son kiriting men uni juft yoki toq ekanligini aniqlab beraman"
+// );
+// son = son % 10;
+// if (son % 2 == 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 9 - TAYYOR
+// let son = +prompt("Son kiriting");
+
+// if (son < 1000) {
+//   son = son / 100;
+//   console.log(son);
+// }
+
+// 10 - TAYYOR
+// let a = +prompt("Son kiriting");
+// let b;
+// let c;
+// b = a / 100;
+// c = (a % 100) / 10;
+// a = a % 10;
+
+// if (a > b) {
+//   if (a > c) {
+//     console.log(a);
+//   } else {
+//     console.log(c);
+//   }
+// } else {
+//   console.log(b);
+//   if (b > c) {
+//     console.log(b);
+//   } else {
+//     console.log(c);
+//   }
+// }
